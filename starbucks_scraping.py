fb39ebc3131207f58212e4d3e56e7ba4d050e6e7
Caio <caiodib10@gmail.com>
2024-06-04 01:27:58 -0300

    configurando webdriver
    configurando webdriver

---
+++ starbucks_scraping.py
@@ -0,0 +1,14 @@
+import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from time import sleep
+
+#configurando o webdriver
+navegador = webdriver.Chrome()
+navegador.get('https://www.starbucks.com')
+
+#request para a pagina
+response = requests.get('https://www.starbucks.com')
+resposta_codigo = response.status_code
+print(resposta_codigo)
+
