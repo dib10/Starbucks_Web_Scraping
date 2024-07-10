f500ed1095ef054329ce2e2d53f23667b3a50f76
Caio <caiodib10@gmail.com>
2024-07-10 20:13:00 -0300

    Url nutricional e remoção da categoria merchandise
    Url nutricional e remoção da categoria merchandise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import json
 import requests
+import pandas as pd
 
 # Realizar a requisição HTTP e verificar se foi bem-sucedida
 response = requests.get('https://www.starbucks.com/bff/ordering/menu')
@@ -16,6 +17,8 @@ if response.status_code == 200:
                 resultados.extend(extrair_informacoes(item['children'], novo_caminho))
             if 'products' in item and item['products']:
                 for produto in item['products']:
+                    if 'merchandise' in novo_caminho[0].lower():
+                        continue
                     # Para cada produto, extrai as informações necessárias
                     categoria, subcategoria, subsubcategoria = (novo_caminho + [None, None])[:3]
                     # Se subsubcategoria for None, recebe o valor de subcategoria
@@ -28,7 +31,8 @@ if response.status_code == 200:
                         'Subcategoria': subcategoria,
                         'Subsubcategoria': subsubcategoria,
                         'Nome': produto['name'],
-                        'URL': url_produto  # URL do produto ajustada
+                        'URL': url_produto,  # URL do produto ajustada
+                        'URL nutricional': url_produto + '/nutrition'  # Corrigir o nome da coluna
                     }
                     resultados.append(produto_info)
         return resultados
@@ -36,9 +40,14 @@ if response.status_code == 200:
     # Extrair informações começando do nível mais alto
     resultados = extrair_informacoes(data['menus'])
 
-    # Exibir resultados
-    for resultado in resultados:
-        print(resultado)
 else:
     print(f"Erro ao acessar o JSON: Status code {response.status_code}")
 print(f"Produtos encontrados: {len(resultados)}")
+
+print(f"{resultados[251]}")
+
+# DATAFRAME
+df = pd.DataFrame(resultados)
+df.to_csv('starbucks_menu.csv', index=False, encoding='utf-8-sig')
+# print(df.head())
+
